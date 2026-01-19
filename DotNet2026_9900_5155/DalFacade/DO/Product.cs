87ebb1f23a7ef49e33c98da9785aa71fc63816fd
BatShevagit <sh0548503408@gmail.com>
2026-01-19 22:37:51 +0200

    add records for the data

---
+++ DotNet2026_9900_5155/DalFacade/DO/Product.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace DO;
+public record Product
+    (int Product_Id,
+    string Product_Name,
+    double Price,
+    int Count,
+    categoryProduct category
+     
+    )
+
+{
+
+
+    ///הוא כותב את ה-productid תמיד 0
+    public Product() : this("", 0, 0, categoryProduct.משחקי_חשיבה)
+    {
+
+    }
+    public Product(string product_Name, 
+    double price,
+    int count,
+    categoryProduct category)
+        : this(111, "", 0,0,categoryProduct.משחקי_חשיבה)
+    {
+
+    }
+   
+
+}
